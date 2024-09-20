944e60d08df368aac91058b806cdc574877da958
Brett Baker <GitBaBB@MyBaBB.com>
2024-09-20 08:46:10 -0700

    adj hover on buttons

--- src/components/ExampleButton.jsx
+++ src/components/ExampleButton.jsx
@@ -84,7 +84,7 @@ const ExampleButton = () => {
 
             <a href="https://mybabb.github.io/BasicEmail_gh_pages/">
               <button
-                // ref={el => (Example2 = el)}
+                ref={el => (Example2 = el)}
                 className="exampleButton2
                      relative   inline-flex items-center justify-center
                      rounded-md bg-gradient-to-b from-green-400 to-blue-500 mx-4 px-4 py-2 text:xs xs:text-2xl
@@ -97,7 +97,7 @@ const ExampleButton = () => {
             </a>
             <a href="https://mybabb.github.io/Pergola-Email/ ">
               <button
-                // ref={el => (Example3 = el)}
+                ref={el => (Example3 = el)}
                 className="exampleButton3
                      relative   inline-flex items-center justify-center
                      rounded-md bg-gradient-to-b from-green-400 to-blue-500 mx-4 px-4 py-2 text:xs xs:text-2xl
